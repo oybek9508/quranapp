cc128691b1ec342557cfa01cc70c1f4e9698d25e
Oybek Toshmatov <oybek@marvrus.com>
2022-12-23 18:59:07 +0900

    feat: finish the surah info page

--- src/components/chapters/Info/index.js
+++ src/components/chapters/Info/index.js
@@ -1,34 +1,72 @@
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Grid, Typography, useTheme } from "@mui/material";
 import Image from "next/image";
+import { useRouter } from "next/router";
 import React from "react";
 
 const Info = ({ chapterInfo, chapterResponse }) => {
+  const router = useRouter();
+  const theme = useTheme();
+  const { chapter } = chapterResponse;
   console.log("chapterInfo", chapterInfo);
   return (
-    <Grid>
+    <Grid container sx={{ maxWidth: "80%", pt: 2, mx: "auto" }}>
       <Grid>
-        <Grid>
+        <Box
+          sx={{
+            height: "50px",
+            display: "grid",
+            placeContent: "center",
+            cursor: "pointer",
+            mb: 2,
+            "&:hover": {
+              bgcolor: theme.palette.background.paper,
+              borderRadius: "5px",
+            },
+          }}
+          onClick={() => router.push(`/${chapterInfo.chapterId}`)}
+        >
+          <Typography> {"<-"} Go to Surah</Typography>
+        </Box>
+        <Box>
+          <Image
+            width={200}
+            height={200}
+            component="img"
+            src={
+              chapter?.revelationPlace === "makkah"
+                ? "/assets/images/makkah.jpeg"
+                : "/assets/images/madina.jpeg"
+            }
+            alt={chapter?.revelationPlace}
+          />
+        </Box>
+      </Grid>
+      <Grid sx={{ flex: 1, ml: 2 }}>
+        <Box
+          sx={{
+            width: "100%",
+            height: "60px",
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
           <Box>
-            <Typography> {"<-"} go to surah</Typography>
+            <Typography>Surah {chapter?.transliteratedName}</Typography>
           </Box>
           <Box>
-            <Image
-              width={100}
-              height={100}
-              component="img"
-              src={
-                chapterResponse?.chapter?.revelationPlace === "makkah"
-                  ? "/assets/images/makkah.jpeg"
-                  : "/assets/images/madina.jpeg"
-              }
-              alt={chapterResponse?.chapter?.revelationPlace}
-            />
+            <Typography>Ayahs</Typography>
+            <Typography>{chapter?.versesCount}</Typography>
           </Box>
-        </Grid>
-        <Grid>
-          <Box>Header</Box>
-          <Box>Body</Box>
-        </Grid>
+          <Box>
+            <Typography>Revelation Place</Typography>
+            <Typography>
+              {chapter?.revelationPlace.charAt(0).toUpperCase() +
+                chapter?.revelationPlace.slice(1).toLowerCase()}
+            </Typography>
+          </Box>
+        </Box>
+        <div dangerouslySetInnerHTML={{ __html: chapterInfo.text }} />
       </Grid>
     </Grid>
   );
