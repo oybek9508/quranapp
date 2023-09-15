196439bc7a5bbe84f056b7349035a6c0cf2a45e7
Oybek <oybek@marvrus.com>
2023-09-16 01:16:35 +0900

    fix: fixes build errors verion@0.0.2

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,68 +1,52 @@
-import "../styles/globals.css";
-import { Provider, shallowEqual, useSelector } from "react-redux";
-import { store } from "../redux/store/index";
-// import createEmotionCache from "src/utility/createEmotionCache";
-import { CacheProvider } from "@emotion/react";
-import {
-  ThemeProvider,
-  CssBaseline,
-  useTheme,
-  createTheme,
-} from "@mui/material";
-import { selectTheme } from "src/redux/slices/theme";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import { useMemo } from "react";
-import { getDesignTokens } from "src/styles/theme";
-import { ThemeTypes } from "src/styles/theme/modes";
-import DataContext from "src/context/DataContext";
-import Layout from "src/components/layout/Layout";
-import {
-  AudioPlayerMachineContext,
-  AudioPlayerMachineProvider,
-} from "src/xstate/AudioPlayerMachineContext";
+import { Provider, shallowEqual, useSelector } from "react-redux";
 import AudioPlayer from "src/components/audio/SurahAudioPlayer";
+import Layout from "src/components/layout/Layout";
+import DataContext from "src/context/DataContext";
+import { selectTheme } from "src/redux/slices/theme";
+import { getDesignTokens } from "src/styles/theme";
+import { AudioPlayerMachineProvider } from "src/xstate/AudioPlayerMachineContext";
+import { store } from "../redux/store/index";
+import "../styles/globals.css";
 
 // const clientSideEmotionCache = createEmotionCache();
 
 function App({ props }) {
-  const { Component, pageProps } = props;
-  console.log("pageProps", pageProps);
-  const { chaptersData, chapterId } = pageProps;
-  const { type } = useSelector(selectTheme, shallowEqual);
+	const { Component, pageProps } = props;
+	console.log("pageProps", pageProps);
+	const { chaptersData, chapterId } = pageProps;
+	const { type } = useSelector(selectTheme, shallowEqual);
 
-  const theme = useMemo(
-    () => createTheme(getDesignTokens(type), { mode: type }),
-    [type]
-  );
+	const theme = useMemo(() => createTheme(getDesignTokens(type), { mode: type }), [type]);
 
-  console.log("theme", theme);
-
-  return (
-    <DataContext.Provider value={pageProps?.chaptersData}>
-      <AudioPlayerMachineProvider>
-        <ThemeProvider theme={theme}>
-          <Layout
-            type="back"
-            singleChapter={{
-              ...pageProps?.chaptersData[pageProps?.chapterId],
-              id: pageProps?.chapterId,
-            }}
-          >
-            <CssBaseline />
-            <Component {...pageProps} />
-            <AudioPlayer />
-          </Layout>
-        </ThemeProvider>
-      </AudioPlayerMachineProvider>
-    </DataContext.Provider>
-  );
+	return (
+		<DataContext.Provider value={pageProps?.chaptersData}>
+			<AudioPlayerMachineProvider>
+				<ThemeProvider theme={theme}>
+					<Layout
+						type="back"
+						singleChapter={{
+							...pageProps?.chaptersData[pageProps?.chapterId],
+							id: pageProps?.chapterId,
+						}}
+					>
+						<CssBaseline />
+						<Component {...pageProps} />
+						<AudioPlayer />
+					</Layout>
+				</ThemeProvider>
+			</AudioPlayerMachineProvider>
+		</DataContext.Provider>
+	);
 }
 
 function MyApp(props) {
-  return (
-    <Provider store={store}>
-      <App props={props} />
-    </Provider>
-  );
+	return (
+		<Provider store={store}>
+			<App props={props} />
+		</Provider>
+	);
 }
 
 export default MyApp;
